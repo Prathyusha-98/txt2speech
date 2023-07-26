908628492ede7a187f34aeaef65ad9afa0181d49
Prathyusha <prathyusha@simplemail.ai>
2023-07-26 22:10:29 +0530

    changes

--- file.py
+++ file.py
@@ -1,30 +1,36 @@
-from flask import Flask, request, send_file, jsonify, send_from_directory
-from ttsmms import TTS, download
+import os
+import azure.cognitiveservices.speech as speechsdk
+from flask import Flask, request, jsonify, send_file
 
 app = Flask(__name__)
+
+# Replace these with your Azure Speech Service credentials
+SUBSCRIPTION_KEY = "b771e7fc717c426d87afa4391f0e691d"
+REGION = "eastus"
 
 @app.route('/synthesize', methods=['POST'])
 def synthesize():
     data = request.get_json()
     text = data['text']
-    
-    dir_path = download("eng", "./data")
-    tts = TTS(dir_path)
-    wav = tts.synthesis(text)
-    wav_path = "example.wav"
-    tts.synthesis(text, wav_path=wav_path)
-    
-#    return send_file(wav_path, mimetype="audio/wav", as_attachment=True)
 
-# Generate URL for the WAV file
-    base_url = request.host_url.rstrip('/')
-    wav_url = f"{base_url}/{wav_path}"
+    print("Received text:", text)
+    # Initialize the Speech Synthesizer
+    speech_config = speechsdk.SpeechConfig(subscription=SUBSCRIPTION_KEY, region=REGION)
+    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
 
-    return jsonify({'wav_url': wav_url})
+    # Synthesize speech from the provided text
+    result = speech_synthesizer.speak_text_async(text).get()
 
-@app.route('/<path:filename>')
-def serve_static_file(filename):
-    return send_from_directory('.', filename)
-    
+    if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+        # Save the synthesized audio to a file (optional)
+        wav_path = "example.wav"
+        with open(wav_path, "wb") as f:
+            f.write(result.audio_data)
+
+        # Return the audio data as a response
+        return send_file(result.audio_data, mimetype="audio/wav")
+    else:
+        return jsonify({'error': 'Speech synthesis failed'}), 500
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8000)
